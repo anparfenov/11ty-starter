f48ec4c29871afbffabcae069d5b6654b7a20af9
Mark Bangert <markbangert@gmail.com>
2022-11-19 21:06:58 +0100

    enable image processing

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,43 @@ const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
 const { markdownLibrary } = require("./.markdown.js");
 const CleanCSS = require('clean-css');
+const Image = require("@11ty/eleventy-img");
+const glob = require("glob-promise");
+const { path } = require("animejs");
+
+const THUMB = 400;
+const FULL = 1200;
+
+async function generateImages() {
+
+	let options = {
+		widths: [THUMB,FULL],
+		formats: ['jpeg'],
+		filenameFormat:function(id, src, width, format, options) {
+			let origFilename = src.split('/').pop();
+			//strip off the file type, this could probably be one line of fancier JS
+			let parts = origFilename.split('.');
+			parts.pop();
+			origFilename = parts.join('.');
+
+			if(width === THUMB) return `thumb-${origFilename}.${format}`;
+			else return `${origFilename}.${format}`;
+		}
+	};
+
+	let files = await glob('**/media/*.{jpg,jpeg,png,gif}');
+	for(const f of files) {
+		console.log('doing f',f);
+
+        let filepathComponents = f.split('/');
+        filepathComponents.pop();
+        filepath = filepathComponents.join('/');
+        options.outputDir = '_site/' + filepath
+
+		let md = await Image(f, options);
+	};
+
+};
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPlugin(pluginRss);
@@ -92,6 +129,12 @@ module.exports = function(eleventyConfig) {
         ui: false,
         ghostMode: false
     });
+
+    eleventyConfig.on('beforeBuild', async () => {
+		console.log('beforeBuild');
+		await generateImages();
+		console.log('images done');
+	});
 
     return {
         templateFormats: [
